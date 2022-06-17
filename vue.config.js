9120fb081fe3c7e2b6df4652ddf1db1a4508fbf9
whenever-start <799091455@qq.com>
2022-06-17 12:58:51 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: '/todo', // 根路径
+  publicPath: '', // 根路径
   outputDir: 'dist', // 构建输出目录，执行：npm run build后项目打包在dist文件下
   assetsDir: 'assets', // 静态资源目录（js,css,img,fonts）
   chainWebpack: (config) => {
